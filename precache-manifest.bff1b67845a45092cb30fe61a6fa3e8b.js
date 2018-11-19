39ec9cbd2c5fca665b7c40c34e2ea7cd0fa6a2d4
Jack Wu <jackwu350@gmail.com>
2018-11-19 13:57:17 -0800

    Updates

---
+++ precache-manifest.bff1b67845a45092cb30fe61a6fa3e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Triangle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c37994b6e65e957e411d",
+    "url": "/Triangle/static/js/runtime~main.c37994b6.js"
+  },
+  {
+    "revision": "7e124fa3a9e4a2a89b47",
+    "url": "/Triangle/static/js/main.7e124fa3.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/Triangle/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "7e124fa3a9e4a2a89b47",
+    "url": "/Triangle/static/css/main.29591db6.chunk.css"
+  },
+  {
+    "revision": "b57586bfb91c249cd2ddbeda6e1c67d5",
+    "url": "/Triangle/index.html"
+  }
+];
