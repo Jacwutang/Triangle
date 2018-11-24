dcebebde50fd83bad3d6850841573505a3df8082
Jack Wu <jackwu350@yahoo.com>
2018-11-23 18:58:40 -0800

    remove render component

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import './calculator.css';
-import TriangleRender from "../triangle_render/triangle_render";
+import React, { Component } from "react";
+import "./calculator.css";
 
 class Calculator extends Component {
   constructor(props) {
@@ -9,8 +8,8 @@ class Calculator extends Component {
       side_one: "",
       side_two: "",
       side_three: "",
-      type : ""
-    }
+      type: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -18,43 +17,56 @@ class Calculator extends Component {
   handleChange(event) {
     let type = event.target.name;
     this.setState({
-      [type] : event.target.value
+      [type]: event.target.value
     });
   }
 
   calculate() {
-    const {side_one, side_two, side_three} = this.state;
+    const { side_one, side_two, side_three } = this.state;
 
-    if(!side_one || !side_two || !side_three) return "";
+    if (!side_one || !side_two || !side_three) return "";
 
-    if(side_one === side_two && side_one === side_three) {
-      return 'Equilateral';
+    if (side_one === side_two && side_one === side_three) {
+      return "Equilateral";
+    } else if (
+      side_one === side_two ||
+      side_one === side_three ||
+      side_two === side_three
+    ) {
+      return "Isoceles";
+    } else {
+      return "Scalene";
     }
-    else if(side_one === side_two || side_one === side_three || side_two === side_three) {
-      return 'Isoceles';
-    }
-    else{
-      return 'Scalene'
-    }
-
   }
-
 
   render() {
     console.log(this.state);
     return (
       <div className="">
-        <input type="text" name="side_one" value={this.state.side_one} onChange={this.handleChange} placeholder="Side 1"></input>
-        <input type="text" name="side_two" value={this.state.side_two} onChange={this.handleChange} placeholder="Side 2"></input>
-        <input type="text" name="side_three" value={this.state.side_three} onChange={this.handleChange} placeholder="Side 3"></input>
-        <TriangleRender
-        side_one   = {this.state.side_one}
-        side_two   = {this.state.side_two}
-        side_three = {this.state.side_three}
-        type       = { this.calculate() }
-         />
-
-
+        <input
+          type="text"
+          name="side_one"
+          value={this.state.side_one}
+          onChange={this.handleChange}
+          placeholder="Side 1"
+        />
+        <input
+          type="text"
+          name="side_two"
+          value={this.state.side_two}
+          onChange={this.handleChange}
+          placeholder="Side 2"
+        />
+        <input
+          type="text"
+          name="side_three"
+          value={this.state.side_three}
+          onChange={this.handleChange}
+          placeholder="Side 3"
+        />
+        <div>
+          <h2> The Triangle is: {this.calculate()} </h2>
+        </div>
       </div>
     );
   }
--- src/components/triangle_render/__tests__/triangle_render.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TriangleRender from '../triangle_render';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TriangleRender />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
